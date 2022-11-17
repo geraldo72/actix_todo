b9508da6cf672f91fb258ff4541288cd5b342458
Luís Geraldo <gegeramos72@gmail.com>
2022-11-17 00:10:17 -0300

    simplews

--- src/main.rs
+++ src/main.rs
@@ -10,16 +10,17 @@ async fn greet(req: HttpRequest) -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env::set_var("RUST_LOG", "actix_todo=debug,actix_web=info");
+    let port = env::var("PORT").unwrap_or("8080".to_string()).parse::<u16>().unwrap();
     let server = HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
             .route("/{name}", web::get().to(greet))
     })
-    .bind(("0.0.0.0", 8080))?
+    .bind(("0.0.0.0",port))?
     .run();
 
 
-    eprintln!("Listening on 0.0.0.0:8080");
+    eprintln!("Listening on 0.0.0.0:{}",port);
 
     server.await
 
